c91487f72c5bca5103465d465f71c206d384268b
unknown <zergovski@mail.ru>
2023-06-14 14:08:37 +0800

    'corrections'

--- rbf_svm.py
+++ rbf_svm.py
@@ -10,23 +10,92 @@ Created on Tue Jun 13 12:56:00 2023
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
+from dbfread import DBF
+import pandas as pd
+import os
+import geopandas as gpd
+
+# From pandas import DataFrame
+dbf_n = DBF(os.path.join(os.getcwd(), 'data', 'class_n.dbf'))
+df_n = pd.DataFrame(iter(dbf_n))
+dbf_s = DBF(os.path.join(os.getcwd(), 'data', 'class_s.dbf'))
+df_s = pd.DataFrame(iter(dbf_s))
+
+# Add column with weights and colors
+df_n = df_n.assign(Weight=1, Color='blue')
+df_s = df_s.assign(Weight=-1, Color='orange')
+
+# Extract arrays of points and class labels
+df = pd.concat([df_n[['x', 'y']], df_s[['x', 'y']]]).values
+weight = pd.concat([df_n["Weight"], df_s["Weight"]]).values
+color = pd.concat([df_n["Color"], df_s["Color"]]).values
+
+# Model training SVM using kernels
+model = SVC(kernel='rbf')
+model.fit(df, weight)
+
+# Grid generation for constructing a decision boundary
+x_min, x_max = df[:, 0].min() - 1, df[:, 0].max() + 1
+y_min, y_max = df[:, 1].min() - 1, df[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), 
+                     np.arange(y_min, y_max, 0.1))
+
+# Predict class labels for each point in the grid
+line = model.predict(np.c_[xx.ravel(), yy.ravel()])
+line = line.reshape(xx.shape)
 
 
+# Gridline creation
+# New DataFrame creation
+line_coords = np.column_stack((xx.ravel(), yy.ravel(), line.ravel()))
+df_line = pd.DataFrame(line_coords, columns=['x', 'y', 'line'])
+df_new = pd.DataFrame(columns=['x', 'y', 'line'])
+df_new.loc[ len(df_new.index )] = [df_line.iloc[0]['x'], df_line.iloc[0]['y'], 
+                                   df_line.iloc[0]['line']]
+
+# Save grid values to the line values
+temp_num = 0
+for i in range(len(df_line)):
+    if df_line.iloc[i]['line'] != temp_num:
+        #add row to end of DataFrame
+        df_new.loc[ len(df_new.index )] = [df_line.iloc[i]['x'], 
+                                           df_line.iloc[i]['y'], 
+                                           df_line.iloc[i]['line']]
+        temp_num = df_line.iloc[i]['line']
+df_new = df_new.loc[df_new['x'] > df_line.iloc[0]['x']]
 
 
-
-
-
-# Визуализация данных и разделяющей поверхности
-plt.figure(figsize=(8, 6))
-plt.contourf(xx, yy, Z, alpha=0.8, cmap='coolwarm')
-plt.scatter(x1[:, 0], x1[:, 1], c='red', label='Class 1')
-plt.scatter(x2[:, 0], x2[:, 1], c='blue', label='Class 2')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('SVM with Gaussian Kernel')
+# Data visualization
+plt.figure(figsize=(24, 16))
+plt.scatter(df[:, 0], df[:, 1], c=color)
+plt.scatter(df_new['x'], df_new['y'], color='red', s=200) # drow border points
+# plt.contourf(xx, yy, line, alpha=0.4, cmap='bwr') # drow the grid
+plt.contour(xx, yy, line, colors='green') # drow the line
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xlabel('x', fontsize=25)
+plt.ylabel('y', fontsize=25)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('SVM with Gaussian Kernel', fontsize=25)
 plt.legend()
 plt.grid(True)
 
-# Отображение графика
 plt.show()
+
+
+# EXPORT TO THE SHP
+# Create a GeoDataFrame from the DataFrame
+# gdf = gpd.GeoDataFrame(df_new, geometry=gpd.points_from_xy(df_new['x'], 
+#                                                            df_new['y']))
+# # Define the projection
+# crs = 'GCS_Mars_2000'
+# # Save the GeoDataFrame as a shapefile
+# gdf.to_file(os.path.join(os.getcwd(), 'data/output/', 'rbf_svm.shp'), 
+#             driver='ESRI Shapefile', crs=crs)
+
+# EXPORT DataFrame TO CSV
+#df_new.to_csv(os.path.join(os.getcwd(), 'data/output/', 'line.csv'), 
+#              index= True )
--- sigmoid_svm.py
+++ sigmoid_svm.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Nonlinear classification with Support Vector Machines (SVM) approach
-with Polynomial Kernel (poly) transformation
+with Sigmoid kernel (sigmoid) transformation
 
-Created on Thu Jun  8 09:47:23 2023
+This classification is not working well yet.
+I need to figure out what parameters could be set for the gamma and coef0
+
+Created on Wed Jun 14 10:54:25 2023
 @author: SergKrasilnikov
 """
 
@@ -13,7 +16,8 @@ from sklearn.svm import SVC
 from dbfread import DBF
 import pandas as pd
 import os
-import geopandas as gpd
+#from sklearn.model_selection import GridSearchCV
+#import geopandas as gpd
 
 # From pandas import DataFrame
 dbf_n = DBF(os.path.join(os.getcwd(), 'data', 'class_n.dbf'))
@@ -30,21 +34,22 @@ df = pd.concat([df_n[['x', 'y']], df_s[['x', 'y']]]).values
 weight = pd.concat([df_n["Weight"], df_s["Weight"]]).values
 color = pd.concat([df_n["Color"], df_s["Color"]]).values
 
+
+
 # Model training SVM using kernels
-# Degree of the polynomial is a parameter that determines the complexity of the
-# decision boundary
-model = SVC(kernel='poly', degree=15)
+model = SVC(kernel='sigmoid', gamma=0.001, coef0=-100)
 model.fit(df, weight)
 
 # Grid generation for constructing a decision boundary
 x_min, x_max = df[:, 0].min() - 1, df[:, 0].max() + 1
 y_min, y_max = df[:, 1].min() - 1, df[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), 
-                     np.arange(y_min, y_max, 0.1))
+                      np.arange(y_min, y_max, 0.1))
 
 # Predict class labels for each point in the grid
 line = model.predict(np.c_[xx.ravel(), yy.ravel()])
 line = line.reshape(xx.shape)
+
 
 # Gridline creation
 # New DataFrame creation
@@ -52,7 +57,7 @@ line_coords = np.column_stack((xx.ravel(), yy.ravel(), line.ravel()))
 df_line = pd.DataFrame(line_coords, columns=['x', 'y', 'line'])
 df_new = pd.DataFrame(columns=['x', 'y', 'line'])
 df_new.loc[ len(df_new.index )] = [df_line.iloc[0]['x'], df_line.iloc[0]['y'], 
-                                   df_line.iloc[0]['line']]
+                                    df_line.iloc[0]['line']]
 
 # Save grid values to the line values
 temp_num = 0
@@ -60,39 +65,57 @@ for i in range(len(df_line)):
     if df_line.iloc[i]['line'] != temp_num:
         #add row to end of DataFrame
         df_new.loc[ len(df_new.index )] = [df_line.iloc[i]['x'], 
-                                           df_line.iloc[i]['y'], 
-                                           df_line.iloc[i]['line']]
+                                            df_line.iloc[i]['y'], 
+                                            df_line.iloc[i]['line']]
         temp_num = df_line.iloc[i]['line']
 df_new = df_new.loc[df_new['x'] > df_line.iloc[0]['x']]
 
+
 # Data visualization
 plt.figure(figsize=(24, 16))
-plt.scatter(df[:, 0], df[:, 1], c=color) # all points
+plt.scatter(df[:, 0], df[:, 1], c=color)
 plt.scatter(df_new['x'], df_new['y'], color='red', s=200) # drow border points
+plt.contourf(xx, yy, line, alpha=0.4, cmap='bwr') # drow the grid
 plt.contour(xx, yy, line, colors='green') # drow the line
-#plt.contourf(xx, yy, line, alpha=0.1, cmap='bwr') # drow the grid
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
 plt.xlabel('x', fontsize=25)
 plt.ylabel('y', fontsize=25)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
-plt.title('Nonlinear classification with support vector machine', fontsize=25)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('SVM with Gaussian Kernel', fontsize=25)
+plt.legend()
 plt.grid(True)
 
 plt.show()
 
 
-# EXPORT TO THE SHP
-# Create a GeoDataFrame from the DataFrame
-gdf = gpd.GeoDataFrame(df_new, geometry=gpd.points_from_xy(df_new['x'], 
-                                                           df_new['y']))
-# Define the projection
-crs = 'GCS_Mars_2000'
-# Save the GeoDataFrame as a shapefile
-gdf.to_file(os.path.join(os.getcwd(), 'data/output/', 'output.shp'), 
-            driver='ESRI Shapefile', crs=crs)
 
-# EXPORT DataFrame TO CSV
-#df_new.to_csv(os.path.join(os.getcwd(), 'data/output/', 'line.csv'), 
-#              index= True )
+
+
+
+# TEST BETTER PARAMETERS
+# # Создание экземпляра модели SVM с Sigmoid Kernel
+# model = SVC(kernel='sigmoid')
+
+# # Определение сетки параметров для перебора
+# param_grid = {
+#     'C': [0.1, 1.0, 10.0, 100.0, 1000.0, 100000.0],
+#     'gamma': [0.001, 0.01, 0.1, 1.0, 10.0],
+#     'coef0': [-100.0, -10.0, 1.0, 10.0, 100.0]
+# }
+
+# # Создание экземпляра GridSearchCV
+# grid_search = GridSearchCV(model, param_grid, cv=5)
+
+# # Подгонка модели на тренировочных данных
+# grid_search.fit(df, weight)
+
+# # Вывод наилучших параметров
+# print("Best Parameters: ", grid_search.best_params_)
+
+# # Оценка производительности модели на тестовых данных
+# accuracy = grid_search.score(df, weight)
+# print("Accuracy: ", accuracy)
